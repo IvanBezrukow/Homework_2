8b6675b7aad89d90feaf19c23a0e39a4919647d1
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-27 23:54:22 +0300

    completed the second task

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -16,10 +16,10 @@ try
 {
     Console.WriteLine("Введите 3-x значное число");
     int num1 = Convert.ToInt32(Console.ReadLine());
-    if (num1 > 99 && num1 < 1000) 
-    Console.WriteLine($"Вторая цифра = {SecondNumber(num1)}");
-    else 
-    Console.WriteLine("Число должно быть трехзначным");
+    if (num1 > 99 && num1 < 1000)
+        Console.WriteLine($"Вторая цифра = {SecondNumber(num1)}");
+    else
+        Console.WriteLine("Число должно быть трехзначным");
 }
 catch
 {
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int ThirdNumber(int num)
+{
+    int result = 0;
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    result = num % 10;
+    return result;
+}
+
+
+
+
+try
+{
+    Console.Write("Введите 3-х значное число = ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    if (num1 < 1000)
+        if (num1 < 99)
+        {
+            Console.WriteLine("Третье цифры нет");
+        }
+        else Console.WriteLine($"Третье число = {ThirdNumber(num1)}");
+    else
+        Console.WriteLine("Необходимо ввести 3-х значное число");
+}
+catch
+{
+    Console.WriteLine("Можно вводить только числа");
+}
