1ed1829f3422a45289899abe9e298a943d48140d
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-27 21:09:14 +0300

    completed the first task via method

---
+++ Task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿int SecondNumber(int num)
+{
+    int result = 0;
+    while (num > 100)
+    {
+        num = num / 10;
+    }
+    result = num % 10;
+    return result;
+}
+
+
+
+
+try
+{
+    Console.WriteLine("Введите 3-x значное число");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    if (num1 > 99 && num1 < 1000) 
+    Console.WriteLine($"Вторая цифра = {SecondNumber(num1)}");
+    else 
+    Console.WriteLine("Число должно быть трехзначным");
+}
+catch
+{
+    Console.WriteLine("Необходимо ввести целое число");
+}
